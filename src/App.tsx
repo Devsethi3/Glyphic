63a64c13952b73133d69112ffaf9215e686dd5c4
Devsethi3 <devsethi4522@gmail.com>
2026-03-08 19:00:36 +0530

    feat: implement dark mode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/landing";
 import { EditorPage } from "./pages/editor";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/editor" element={<EditorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/editor" element={<EditorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
